8e6b927e695b5d6fa8dcd9de2547957d81402a67
Ngina07 <phillis.njoroge@strathmore.edu>
2019-08-19 00:47:37 +0300

    convert user birthdat details to numbers from strings

--- akan-names.js
+++ akan-names.js
@@ -1,103 +1,110 @@
 //this function will validate gender, day, year, month
 function validateForm() {
-    var gender = document.getElementsByName("gender");
-    var mdate = document.getElementById("date");
-    var mmonth = document.getElementById("month");
-    var myear = document.getElementById("year");
+    var usergender = document.getElementsByName("gender");
+    var userdate = document.getElementById("date");
+    var usermonth = document.getElementById("month");
+    var useryear = document.getElementById("year");
     var formValid = false;
     var i = 0;
 
-	//tests
-	//comment then when you see them working correcly
-	alert(gender);
-	alert(mdate);
-	alert(mmonth);
-	alert(myear);
-
-
-
-    if (mdate.value == "" || mdate.value == null) {
-	alert("Please Input date");
+    if (userdate.value == "" || userdate.value == null) {
+        // alert("Please Input date");
+        document.getElementById("error").innerHTML = "Day is Empty"
+        document.getElementById("error").style.color = "red"
+        userdate.style.border = "2px solid red"
         return false;
     } else {
-        mdate.style.border = "";
-        if (!isNaN(mdate.value)) {
-            if (mdate.value <= 0 || mdate.value > 31) {
-		alert("Invalid date");
+       
+        if (!isNaN(userdate.value)) {
+            if (userdate.value <= 0 || userdate.value > 31) {
+                document.getElementById("error").innerHTML = "Invalid day"
+                document.getElementById("error").style.color = "red"
+                userdate.style.border = "2px solid red"
                 return false;
+            } else {
+
+                userdate.style.border = "2px solid green"
+                date = parseInt(userdate.value);
             }
         } else {
-		alert("Date must be a numbre");
-            return false;
-        }
-    }
-    if (mmonth.value == "" || mmonth.value == null) {
-	alert("Please Input month");
-        return false;
-    } else {
-        if (!isNaN(mmonth.value)) {
-            if (mmonth.value <= 0 || mmonth.value > 12) {
-		alert("Invalid month");	
-                return false;
-            }
-        } else {
-	alert("Month must be a number");
-            return false;
-        }
-    }
-    if (myear.value == "" || myear.value == null) {
-	alert("Please Input year");
-        return false;
-    } else {
-        myear.style.border = "";
-        if (!isNaN(myear.value)) {
-            if (myear.value.length != 4) {
-                alert("Invalid year");
-                return false;
-            }
-        } else {
-	alert("Year must be a number");
+            document.getElementById("error").innerHTML = "Day must be a number"
+            document.getElementById("error").style.color = "red"
+            userdate.style.border = "2px solid red"
             return false;
         }
     }
 
-    while (!formValid && i < gender.length) {
-        if (gender[i].checked)
+
+
+    if (usermonth.value == "" || usermonth.value == null) {
+        document.getElementById("error").innerHTML = "Month is Empty"
+        document.getElementById("error").style.color = "red"
+        usermonth.style.border = "2px solid red"
+        return false;
+    } else {
+        if (!isNaN(usermonth.value)) {
+            if (usermonth.value <= 0 || usermonth.value > 12) {
+                document.getElementById("error").innerHTML = "Invalid month"
+                document.getElementById("error").style.color = "red"
+                usermonth.style.border = "2px solid red"
+                return false;
+            } else {
+                usermonth.style.border = "2px solid green"
+                month = parseInt(usermonth.value);
+            }
+        } else {
+            document.getElementById("error").innerHTML = "Month must be a number"
+            document.getElementById("error").style.color = "red"
+            usermonth.style.border = "2px solid red"
+            return false;
+        }
+    }
+
+
+    if (useryear.value == "" || useryear.value == null) {
+        document.getElementById("error").innerHTML = "Year is Empty"
+        document.getElementById("error").style.color = "red"
+        useryear.style.border = "2px solid red"
+        return false;
+    } else {
+        useryear.style.border = "";
+        if (!isNaN(useryear.value)) {
+            if (useryear.value.length != 4) {
+                document.getElementById("error").innerHTML = "Invalid year"
+                document.getElementById("error").style.color = "red"
+                useryear.style.border = "2px solid red"
+                return false;
+            } else {
+                document.getElementById("error").innerHTML = ""
+                useryear.style.border = "2px solid green"
+                year = parseInt(useryear.value);
+            }
+        } else {
+            document.getElementById("error").innerHTML = "Year must be a number"
+            document.getElementById("error").style.color = "red"
+            useryear.style.border = "2px solid red"
+            return false;
+        }
+    }
+
+    while (!formValid && i < usergender.length) {
+        if (usergender[i].checked) {
+
+            gender = usergender[i].value;
+            document.getElementById("my_gender").style.color = ""
             formValid = true;
+        }
         i++;
     }
     if (!formValid) {
-       alert("Select gender");
+        document.getElementById("my_gender").style.color = "red"
+
         return false;
     }
-
-
-    return formValid;
-
-}
-
-
-//functions to get user details
-function getUserDetails(){
-
-
-}
-
-
-
-//this function will contain all function which you will write. They will be run simultaneoulsy
-
-function aggregateMyFunctions(){
-var isValid = validateForm() ;
-
-if(!isValid){
-	validateForm() ;
-	return false;
-}else{
-	//all other functions
-	//update this functions af you write more functions
-	//example 
-	getUserDetails();
-	return false;
-}
-
+    var validatedDetails = {
+        gender: gender,
+        date: date,
+        month: month,
+        year: year,
+        valid:formValid
+    }
